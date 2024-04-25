bc183e4ce91c5b7643b1c173d5def40e9ec8046c
ashikisu <ashik.searchg@gmail.com>
2024-04-25 13:18:25 +0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("home"),
+        title: const Text("homes"),
       ),
     );
   }
